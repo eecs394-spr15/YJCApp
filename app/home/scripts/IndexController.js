5f968962da08c5cb697d6c0535c590488e9ff327
hzn942 <hzn942@gmail.com>
2015-05-11 21:42:56 -0500

    basic home page view

--- app/home/scripts/IndexController.js
+++ app/home/scripts/IndexController.js
@@ -3,4 +3,39 @@ angular
   .controller('IndexController', function($scope, supersonic) {
     // Controller functionality here
     supersonic.ui.navigationBar.hide();
+
+
+    //find jobs that match
+
+  //   if(currUser){
+  //   	var interests = currUser.get("Interests");
+  //   	var zipcode = currUser.get("zipcode");
+  //   	var skills = currUser.get("skills");
+  //   	var degree = currUser.get("degree");
+
+  //   	//start query
+
+
+  //   	var Job = Parse.Object.extend("Job");
+		// var query = new Parse.Query(Job);
+
+		// query.equalTo("zipcode", zipcode);
+		
+
+  //   }
+
+    var Job = Parse.Object.extend("Job");
+    var query = new Parse.Query(Job);
+    
+    query.find({
+      success: function(results) {
+        //alert("Successfully retrieved " + results.length + " scores.");
+        // Do something with the returned Parse.Object values
+        $scope.jobs = results;
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+
 });
--- config/structure.coffee
+++ config/structure.coffee
@@ -5,10 +5,18 @@ module.exports =
   # See styling options for tabs and other native components in app/common/native-styles/ios.css or app/common/native-styles/android.css
   tabs: [
     {
+      title: "Jobs"
+      id: "home"
+      location: "home#index" # Supersonic module#view type navigation
+    }
+
+    {
       title: "My Account"
       id: "account"
       location: "Account#login" # Supersonic module#view type navigation
     }
+
+    
   ]
 
   # rootView:
