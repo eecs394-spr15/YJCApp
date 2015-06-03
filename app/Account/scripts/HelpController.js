ca95a08629afd3b6c6e8c9864ed7fe761ec08db2
Isabella Valdescruz <isabellavaldescruz2016@u.northwestern.edu>
2015-06-03 17:12:35 -0500

    update help page

--- app/Account/scripts/HelpController.js
+++ app/Account/scripts/HelpController.js
@@ -7,15 +7,17 @@ angular
   	slideimages[0] = new Image(); // create new instance of image object
   	slideimages[0].src = "/imags/Welcome.jpg"; // set image src property to image path, preloading image in the process
   	slideimages[1] = new Image();
-  	slideimages[1].src = "/imags/profile.jpg";
+  	slideimages[1].src = "/imags/slide1.jpg";
   	slideimages[2] = new Image();
-  	slideimages[2].src = "/imags/profileEdit.jpg";
+  	slideimages[2].src = "/imags/slide2.jpg";
   	slideimages[3] = new Image();
-  	slideimages[3].src = "/imags/Home.jpg";
+  	slideimages[3].src = "/imags/slide3.jpg";
   	slideimages[4] = new Image();
-  	slideimages[4].src = "/imags/advisor.jpg";
+  	slideimages[4].src = "/imags/slide4.jpg";
   	slideimages[5] = new Image();
-  	slideimages[5].src = "/imags/sort.jpg";
+  	slideimages[5].src = "/imags/slide5.jpg";
+  	slideimages[6] = new Image();
+  	slideimages[6].src = "/imags/slide6.jpg";
 
   	var step=0;
 
